c296a556f09328951f0906aa68db5c9fc7c02131
SarahOtmane <sarahotmane02@gamil.com>
2023-01-28 23:42:31 +0100

    28/01/23

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ import {tagCanvas} from './components/about.js';
 import {portfolioCards} from './components/portfolio.js';
 import {showPopUp} from './components/portfolio-popup.js';
 import {validateForm} from './components/form.js';
+import { movies } from './components/movies.js';
 
 
 
@@ -14,6 +15,7 @@ function run(){
     portfolioCards();
     showPopUp();
     validateForm();
+    movies();
 
     /*************** les animations  **************/
     animSect1();
